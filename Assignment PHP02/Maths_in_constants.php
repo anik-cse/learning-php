75b4a4c5c325f974d3b6f4edd4f84ea1ee083cec
Mir Monoarul Alam <14123130+anik-cse@users.noreply.github.com>
2022-12-17 11:27:34 +0600

    Create Maths_in_constants.php

---
+++ Assignment PHP02/Maths_in_constants.php
@@ -0,0 +1,20 @@
+<?php
+
+#initializing the value of constants 
+define("number_1" , "123");
+define("number_2" , "456");
+define("number_3" , "789");
+
+#calculating addition the constants
+echo "First Number: " . number_1 . " Secound Number: " . number_2 . " Third Number: " . number_3 ." = ". number_1 + number_2 + number_3;
+
+echo "<br/>";
+#calculating subtraction the constants
+echo "First Number: " . number_1 . " Secound Number: " . number_2 . " Third Number: " . number_3 ." = ". number_1 - number_2 - number_3;
+
+
+echo "<br/>";
+#calculating multiplication the constants
+echo "First Number: " . number_1 . " Secound Number: " . number_2 . " Third Number: " . number_3 ." = ". number_1 * number_2 * number_3;
+
+?>
