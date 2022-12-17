b8ad18cbc16011be5c9e448b315bfd5fe0404015
Mir Monoarul Alam <14123130+anik-cse@users.noreply.github.com>
2022-12-17 22:31:01 +0600

    Create loopinarray.php

---
+++ Assignment PHP05/loopinarray.php
@@ -0,0 +1,38 @@
+<h1> Array with while loop</h1>
+
+<?php
+$array_data = ["Eggs", "Bacon", "HashBrowns", "Beans", "Bread", "RedSauce"];
+$arrayLen = count($array_data);
+
+$i = 0;
+while ($i < $arrayLen) {
+    echo $array_data[$i] . "<br />";
+
+    $i++;
+}
+?>
+
+
+<h1> Array with for loop</h1>
+
+<?php for ($i = 0; $i < $arrayLen; $i++) {
+    echo $array_data[$i] . "<br />";
+} ?>
+
+<h1> Array with foreach loop</h1>
+
+<?php foreach ($array_data as $data) {
+    echo $data . "<br />";
+} ?>
+
+<h1> Array with DoWhile loop</h1>
+
+<?php $k = 0;
+
+do {
+    echo $array_data[$k] . "<br />";
+    $k++;
+} while ($k < $arrayLen);
+
+
+?>
